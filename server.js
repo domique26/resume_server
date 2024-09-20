d1e7a40915461675dab807c45e76dbf5d7893be9
Kozlowski <dki@sybit.de>
2024-09-20 12:59:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,14 @@ app.use(cors());
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('hello world') 
 });
-
-
 
 mongoose.connect(`mongodb+srv://domikkozlowski26:${process.env.MONGO_DB_PASSWORD}@backenddb.eyxmv.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`)
 .then(() => {
   console.log("Connected to the database");
-  app.listen(3000, () => {
-    console.log(`Server is running. http://localhost:3000/`)
+  app.listen(8000, () => {
+    console.log(`Server is running. http://localhost:8000/`)
   });
 })
 .catch((err) => {
